5a26f4d9f53fe245d4b9ed6cb205c0eebbb3334e
Triston Burns <tristonburns97@gmail.com>
2022-03-08 12:50:28 -0700

    Update index.html template script source

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: {

3256276a49767dbbee2173c28534000270d3c3da
Triston Burns <tristonburns97@gmail.com>
2022-03-08 10:23:08 -0700

    Install html-webpack-plugin && change to production mode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-console.log(process.env.NODE_ENV);
-
-// if (provess.env.NODE_ENV !== 'production') {
-//     console.log('In development mode.');
-// };
-
 ReactDOM.render(
     <React.StrictMode>
         <App />
---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env", "@babel/preset-react"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+        }),
+    ],
+    output: {
+        filename: 'app.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
